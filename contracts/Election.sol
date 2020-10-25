b79d8b48dbff94deff5e1e20152b495b3c405382
Palaash Atri <palaash.atri@gmail.com>
2020-10-25 19:54:55 +0530

    complete client-side voting 🎊

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -14,6 +14,9 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
+    event votedEvent (
+        uint indexed _candidateId
+    );
     constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
@@ -25,17 +28,20 @@ contract Election {
     }
     
     function vote (uint _candidateId) public {
-        // require that they haven't voted before
-        require(!voters[msg.sender]);
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
 
-        // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // record that voter has voted
-        voters[msg.sender] = true;
+    // record that voter has voted
+    voters[msg.sender] = true;
 
-        // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
+    // update candidate vote Count
+    candidates[_candidateId].voteCount ++;
+
+    // trigger voted event
+    emit votedEvent(_candidateId);
     }
 }
 
